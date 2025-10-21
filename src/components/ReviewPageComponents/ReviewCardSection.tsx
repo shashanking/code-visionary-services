d39828b34d531a6c431c982eb35cd974ee536bcf
ami-pbj <purnendubikash.21@gmail.com>
2025-10-21 20:47:33 +0530

    added review card

---
+++ src/components/ReviewPageComponents/ReviewCardSection.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import SectionContainer from "../shared/SectionContainer";
+import ContentContainer from "../shared/ContentContainer";
+import ReviewCardBg from "../../assets/review-page/review-bg.jpg";
+
+const ReviewCardSection: React.FC = () => {
+  return (
+    <SectionContainer
+      id="review-card"
+      fullWidth
+      padding="lg"
+      background="#e3e3e3"
+      className="relative min-h-screen overflow-hidden flex flex-col items-center justify-center text-center"
+    >
+      <div className="absolute inset-0 z-0 overflow-hidden">
+        <div
+          className="w-full h-full bg-cover bg-no-repeat bg-center"
+          style={{ backgroundImage: `url(${ReviewCardBg})` }}
+        />
+
+        <div className="absolute inset-0 bg-gradient-to-b from-[#F0F0F0] via-[#F0F0F0]/0 to-[#F0F0F0] z-0 pointer-events-none" />
+      </div>
+
+      <ContentContainer
+        maxWidth="7xl"
+        paddingX="lg"
+        className="relative z-10 py-10 flex flex-col justify-center items-center text-center overflow-hidden"
+      >
+        <div className="relative w-full max-w-2xl pt-20 pb-10 flex flex-col justify-start items-center">
+          <h1 className="font-heading font-bold text-title-lg text-center uppercase mb-10 leading-tight max-w-md text-[#161616] leading-[1.1]">
+            What Our{" "}
+            <span className="bg-gradient-to-l from-[#B5442C] to-[#4F1E13] bg-clip-text text-transparent">
+              Clients
+            </span>{" "}
+            Say
+          </h1>
+
+          <p className="font-sans font-normal text-body1 text-[#161616] max-w-lg mx-auto leading-[1.5]">
+            Discover how Code Visionary Studios has made a difference for our
+            clients. Our testimonials showcase the positive impact of our web
+            design, app development, branding, and digital marketing services.
+          </p>
+        </div>
+      </ContentContainer>
+    </SectionContainer>
+  );
+};
+
+export default ReviewCardSection;
--- src/pages/ReviewPage/ReviewPage.tsx
+++ src/pages/ReviewPage/ReviewPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroReviewPage from "../../components/ReviewPageComponents/HeroReviewPage";
+import ReviewCardSection from "../../components/ReviewPageComponents/ReviewCardSection";
 
 const ReviewPage = React.memo(() => {
   return (
     <div>
       <HeroReviewPage />
+      <ReviewCardSection />
     </div>
   );
 });
