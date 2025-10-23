afabba429f98b6c25e7ab9d0f2b5bb1d5fc30a3d
ami-pbj <purnendubikash.21@gmail.com>
2025-10-23 19:06:55 +0530

    removed un-used scroll to top functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import AppRoutes from "./routes/AppRoutes";
-import { useLocation } from "react-router-dom";
-
-const ScrollToTop: React.FC = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }, [pathname]);
-
-  return null;
-};
 
 const App: React.FC = () => {
   return (
     <>
-      {/* <ScrollToTop /> */}
       <AppRoutes />
     </>
   );
