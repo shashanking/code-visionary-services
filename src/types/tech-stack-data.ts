80400c92e6117405c969a3270f663668c79ae143
ami-pbj <purnendubikash.21@gmail.com>
2025-11-05 17:39:23 +0530

    added custom hook for tech stack section to call the sanity data

---
+++ src/types/tech-stack-data.ts
@@ -0,0 +1,57 @@
+// Sanity-specific types
+export interface SanityTechCategory {
+  _id: string;
+  name: string;
+  image?: {
+    asset: {
+      url: string;
+    };
+    alt?: string;
+  };
+  highlighted: boolean;
+  order: number;
+}
+
+export interface SanityTechnology {
+  _id: string;
+  name: string;
+  image: {
+    asset: {
+      url: string;
+    };
+    alt?: string;
+  };
+  active: boolean;
+  category: {
+    _id: string;
+    name: string;
+    highlighted: boolean;
+  };
+}
+
+export interface TechCategory {
+  id: string;
+  name: string;
+  image?: string;
+  imageAlt?: string;
+  highlighted: boolean;
+  order: number;
+}
+
+export interface Technology {
+  id: string;
+  name: string;
+  image: string;
+  imageAlt?: string;
+  active: boolean;
+  category: {
+    id: string;
+    name: string;
+    highlighted: boolean;
+  };
+}
+
+export interface TechStackData {
+  categories: TechCategory[];
+  technologies: Technology[];
+}
