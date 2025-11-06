dec4082cf84084827c3249061d37dc5c82976d13
ami-pbj <purnendubikash.21@gmail.com>
2025-11-06 19:44:18 +0530

    addded challenges and services section

--- src/pages/PortfolioDetailPage/PortfolioDetailPage.tsx
+++ src/pages/PortfolioDetailPage/PortfolioDetailPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import HeroPortfolioDetailsPage from "../../components/PortfolioDetailPage/HeroPortfolioDetailsPage";
+import HeroPortfolioDetailsPage from "../../components/PortfolioDetailPage/HeroSection";
+import ChallengeSection from "../../components/PortfolioDetailPage/ChallengeSection";
 
 const PortfolioDetailPage = React.memo(() => {
   return (
     <div>
       <HeroPortfolioDetailsPage />
+      <ChallengeSection />
     </div>
   );
 });
