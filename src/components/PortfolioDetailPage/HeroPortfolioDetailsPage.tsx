13c4a1d1242f91b21c2395537ee03525e84b3c48
ami-pbj <purnendubikash.21@gmail.com>
2025-11-06 18:39:54 +0530

    updated portfolio hero section

--- src/components/PortfolioDetailPage/HeroPortfolioDetailsPage.tsx
+++ src/components/PortfolioDetailPage/HeroPortfolioDetailsPage.tsx
@@ -76,15 +76,17 @@ const HeroPortfolioDetailsPage: React.FC = () => {
             style={{ backgroundImage: `url(${PortfolioHeroBg})` }}
             aria-hidden="true"
           />
+
+          <div className="absolute bottom-0 left-0 w-full h-1/4 bg-gradient-to-t from-[#F0F0F0] to-transparent z-0 pointer-events-none" />
         </div>
 
         <div className="relative z-10 w-full max-w-2xl flex flex-col justify-center items-center">
-          <h1 className="font-heading font-bold text-4xl md:text-6xl lg:text-7xl uppercase mb-6 leading-tight">
+          <h1 className="font-heading font-semibold text-title-sm uppercase mb-6 leading-[1.5] bg-gradient-to-t from-[#4F1E13] to-[#B5442C] bg-clip-text text-transparent">
             {title}
           </h1>
 
           {subtitle && (
-            <h2 className="text-title-2xl font-light mb-6 opacity-90">
+            <h2 className="relative w-full max-w-2xl mx-auto text-center text-[#161616] leading-[1.1] text-title-2xl font-[700] mb-6">
               {subtitle}
             </h2>
           )}
@@ -92,6 +94,33 @@ const HeroPortfolioDetailsPage: React.FC = () => {
           <p className="font-sans font-normal text-body1 text-[#161616] leading-[1.5] max-w-xl  mx-auto">
             {hero.description}
           </p>
+
+          <div className="w-full max-w-xl mx-auto grid grid-cols-1 sm:grid-cols-3 gap-2 sm:gap-10 mt-10">
+            <div className="mb-4">
+              <div className="font-heading text-[#161616] font-medium text-body1 uppercase">
+                Client
+              </div>
+              <div className="text-[#303030] font-[300] text-body1">
+                {hero.client}
+              </div>
+            </div>
+            <div className="mb-4">
+              <div className="font-heading text-[#161616] font-medium text-body1 uppercase">
+                Role
+              </div>
+              <div className="text-[#303030] font-[300] text-body1">
+                {hero.role}
+              </div>
+            </div>
+            <div className="mb-4">
+              <div className="font-heading text-[#161616] font-medium text-body1 uppercase">
+                Date
+              </div>
+              <div className="text-[#303030] font-[300] text-body1">
+                {hero.date}
+              </div>
+            </div>
+          </div>
         </div>
       </ContentContainer>
 
