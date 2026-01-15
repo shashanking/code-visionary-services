519119f6fce756b2b8f21b2436e8472bc2cd20e1
Klnishant <nishantkaushal075@gmail.com>
2026-01-15 19:33:16 +0530

    Twak widgets added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
+import TawkChat from "./services/TwakMessanger/TwakMessanger";
 
 const App: React.FC = () => {
   return (
     <>
       <AppRoutes />
+      <TawkChat />
     </>
   );
 };
