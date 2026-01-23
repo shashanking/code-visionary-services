3576245c97447da57182c16f6ae111d8df43a262
Klnishant <nishantkaushal075@gmail.com>
2026-01-23 23:42:17 +0530

    Latency reduced

--- src/services/TwakMessanger/TwakMessanger.tsx
+++ src/services/TwakMessanger/TwakMessanger.tsx
@@ -1,34 +1,20 @@
 "use client";
 
-import { useEffect } from "react";
-
-declare global {
-  interface Window {
-    Tawk_API?: any;
-    Tawk_LoadStart?: Date;
-  }
-}
+import Script from "next/script";
 
 export default function TawkChat() {
-  useEffect(() => {
-    if (window.Tawk_API) return;
+  return (
+    <>
+      {/* DNS + TLS warm-up */}
+      <link rel="dns-prefetch" href="https://embed.tawk.to" />
+      <link rel="preconnect" href="https://embed.tawk.to" crossOrigin="" />
 
-    window.Tawk_API = window.Tawk_API || {};
-    window.Tawk_LoadStart = new Date();
-
-    const s = document.createElement("script");
-    s.src = 'https://embed.tawk.to/6968c557c03c961980f0097f/1jf0k5nj7';
-    s.async = true;
-    s.charset = "UTF-8";
-    s.setAttribute("crossorigin", "*");
-
-    document.body.appendChild(s);
-
-    return () => {
-      document.body.removeChild(s);
-    };
-  }, []);
-
-  return null;
+      {/* Load Tawk as early as possible */}
+      <Script
+        id="tawk-chat"
+        strategy="afterInteractive"
+        src="https://embed.tawk.to/6968c557c03c961980f0097f/1jf0k5nj7"
+      />
+    </>
+  );
 }
-
